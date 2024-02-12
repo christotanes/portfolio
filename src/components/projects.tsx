740f52bc76dd78cface1920fbb9e48589308ea02
christopher.totanes <christopher.totanes@gmail.com>
2024-02-12 17:16:43 +0800

    Add projects template and projects temporary database

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,18 +1,45 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Image, Button } from "react-bootstrap";
+import projects from "../data/projects";
 
 export default function Projects() {
 
-	const projectTitle: string = "";
-	const projectDescription: string = "";
+	const projectsTable = projects.map((project) => (
+		<Row key={project.id}>
+			<Col xs={5}>
+				<h4 className="pt-3">{project.title}</h4>
+				<p className="mt-3 pe-3">{project.description}</p>
+				<Button href={project.link} target="_blank">{ project.linkMsg }</Button>
+			</Col>
+			<Col xs={5} className="d-flex flex-column align-items-center">
+				<div className={`${'slideShow' + project.id}`}>
+					<Image src={project.image1} className="w-100" alt={project.alt} />
+				</div>
+				<div className={`${'slideShow' + project.id}`}>
+					<Image src={project.image2} className="w-100" alt={project.alt} />
+				</div>
+				<div className={`${'slideShow' + project.id}`}>
+					<Image src={project.image3} className="w-100" alt={project.alt} />
+				</div>
+			</Col>
+			<Col xs={1}>
+				<div>
+					<Image src={project.image1} className="w-100" alt={project.alt} />
+				</div>
+				<div>
+					<Image src={project.image2} className="w-100" alt={project.alt} />
+				</div>
+				<div>
+					<Image src={project.image3} className="w-100" alt={project.alt}
+					/>
+				</div>
+			</Col>
+		</Row>
+	));
 
 	return (
 		<Container>
-			<h2>Projects</h2>
-			<Row>
-				<Col>
-					
-				</Col>
-			</Row>
+			<h2 className="pt-3">Projects</h2>
+			{ projectsTable }
 		</Container>
 	)
 }
---
+++ src/data/projects.tsx
@@ -0,0 +1,32 @@
+import ptgame1 from "../images/pt-game-1.png";
+import ptgame2 from "../images/pt-game-2.png";
+import ptgame3 from "../images/pt-game-3.png";
+import postgresql1 from "../images/postgresql-api-2.png";
+import postgresql2 from "../images/postgresql-api-4.png";
+import postgresql3 from "../images/postgresql-api-5.png";
+
+const projects = [
+	{
+		"id": 1,
+		"title": "Current Project",
+		"description": "Designing a JavaScript multiplayer game that will test Physical Therapist students of their knowledge on certain subjects.",
+		"link": "https://github.com/christotanes/pt-game",
+		"linkMsg": "View project on GitHub",
+		"image1": ptgame1,
+		"image2": ptgame2,
+		"image3": ptgame3,
+		"alt": "Current node project game",
+	},
+	{
+		"id": 2,
+		"title": "PostgreSQL NodeJS API",
+		"description": "Developed a RESTful API using Node.js, PostgreSQL database, using a node PostgreSQL framework that implemented CRUD operations, JWT authorization and Bcrypt encryption and exercised object relational management with Mocha and chai to do unit testing.",
+		"link": "https://github.com/christotanes/learning-postgresql-api",
+		"linkMsg": "View project on GitHub",
+		"image1": postgresql1,
+		"image2": postgresql2,
+		"image3": postgresql3,
+		"alt": "PostgreSQL api images",
+	}
+]
+export default projects;
